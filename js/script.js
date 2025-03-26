eacf2c0893855582a073978e7ec39e4d1300e293
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-26 19:14:57 +0100

    [UPD] Card biglietto terminata

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let nameField = document.getElementById('name-field'); // Definisco la variabile nameField che conterrà il nome che l'utente inserisce
 let kmValue = document.getElementById('km'); // Definisco la variabile kmToTravel che conterrà i km che l'utente deve percorrere
 let age = document.getElementById('age'); // Definisco la variabile age che conterrà la scelta effettuata dall'utente
-let btn = document.querySelector('button') // Recupero il tag button e lo metto dentro la variabile btn
+let btnSuccess = document.getElementById('conferma') // Recupero il tag button e lo metto dentro la variabile btn
+
+let results = document.getElementById('result');
 
 let userName = document.getElementById('user-name');
 let ticketDiscount = document.getElementById('ticket-discount');
@@ -16,9 +18,9 @@ const discontSenior = 0.6; // Definisco le varibili per il prezzo al km e per le
 
 let paragraph = document.querySelector('p')
 
-btn.addEventListener('click', function(event){ // Creo l'evento al click del button e ne definisco la funzione
+btnSuccess.addEventListener('click', function(event){ // Creo l'evento al click del button e ne definisco la funzione
     event.preventDefault()
-    let kmToTravel = parseInt(kmValue.value) // Prendo il valore contenuto in kmValue e lo assegno alla variabile kmToTravel passandolo come valore numerico
+    let kmToTravel = parseInt(kmValue.value.trim()) // Prendo il valore contenuto in kmValue e lo assegno alla variabile kmToTravel passandolo come valore numerico
     console.log(nameField.value);
     console.log(kmToTravel);
     console.log(age.value);
@@ -26,22 +28,26 @@ btn.addEventListener('click', function(event){ // Creo l'evento al click del but
     let string
     let price = kmToTravel * priceToKm; // Prezzo finale senza scontische
 
-    userName.innerHTML = nameField.value;
-    trainCar.innerHTML = Math.floor(Math.random * 10 + 1);
-    cpCode.innerHTML = Math.floor(Math.random * 100000 + 1);
+    results.className = results.className.replace(`d-none`, `d-block`)
+
+    userName.innerHTML = nameField.value.trim();
+    trainCar.innerHTML = Math.floor(Math.random() * 10 + 1);
+    cpCode.innerHTML = Math.floor(Math.random() * (100000 - 10000) + 1);
 
     if (age.value.includes('minorenne')){ // Prima condizione se l'utente inserisce minorenne
         let finalPrice = price * discontUnderage; // Prezzo totale * sconto riservato ai minorenni
-        ticketDiscount.innerHTML = `Biglietto scontato del 20%`;
-        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}`;
+        ticketDiscount.innerHTML = `Sconto del 20%`;
+        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}&euro;`;
 
     } else if (age.value.includes('senior')){ // Seconda condizione se l'utente inserisce over 65
         let finalPrice = price * discontSenior; // Prezzo totale * sconto riservato agli over 65
-        ticketDiscount.innerHTML = `Biglietto scontato del 40%`;
-        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}`;
+        ticketDiscount.innerHTML = `Sconto del 40%`;
+        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}&euro;`;
     } else { // Altrimenti se inserisce maggiorenne
         let finalPrice = price; // Prezzo finale senza scontische
         ticketDiscount.innerHTML = `Biglietto standard`;
-        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}`;
+        ticketPrice.innerHTML = `${finalPrice.toFixed(2)}&euro;`;
     }
 })
+
+
