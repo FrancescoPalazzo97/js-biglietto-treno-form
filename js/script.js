4dbec17a8a7a5cb9bbcf533133a7c22e58fee50c
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-26 14:37:15 +0100

    [UPD] Definizione variabili

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let name = document.getElementById('name'); // Definisco la variabile name che conterrà il nome che l'utente inserisce
+let kmToTravel = Document.getElementById('km'); // Definisco la variabile kmToTravel che conterrà i km che l'utente deve percorrere
+let age = document.getElementById('age'); // Definisco la variabile age che conterrà la scelta effettuata dall'utente
